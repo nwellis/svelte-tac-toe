3c474b006b30c9e7147e0b70f73d8df15e2861ca
nwellis <nicholas.w.ellis@gmail.com>
2024-11-10 09:03:42 -0600

    feat: game page

---
+++ src/lib/components/Grid.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type {Player} from '../models/Player'
+  interface Props {
+    cells: Player[]
+    onClick: (cell: number) => void
+  }
+
+  const { cells, onClick }: Props = $props()
+</script>
+
+<div class="w-fit grid grid-cols-3 grid-rows-3">
+  {#each cells as cell, i}
+    <button class="size-24 border border-black" onclick={() => onClick(i)}>
+      {cell?.toUpperCase()}
+    </button>
+  {/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Board } from "$lib/Board.svelte";
+	import Grid from "$lib/components/Grid.svelte";
+
+  const board = new Board('Human')
+</script>
+
+<h1>Svelte-Tac-Toe</h1>
+
+<button
+  type="button"
+  onclick={() => board.reset()}
+>
+  Reset
+</button>
+
+<p>Winner: {board.winner ?? "NONE"}</p>
+
+<Grid 
+  cells={board.cells} 
+  onClick={i => board.play(board.turn, i)} 
+/>
