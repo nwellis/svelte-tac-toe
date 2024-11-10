1cc6fb0e78c78f8f8e33edda22575241ddcdeac8
nwellis <nicholas.w.ellis@gmail.com>
2024-11-10 09:03:34 -0600

    feat: counter page

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { counter } from "$lib";
+</script>
+
+<h1>Svelte-Tac-Toe Counter</h1>
+
+<button
+  type="button"
+  onclick={() => counter.increment()}
+>
+  Count {counter.value}
+</button>
